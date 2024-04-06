022938d10308e495cb6f58cb172221a5b5ad1268
poornima reddy <54319456+Poornimait@users.noreply.github.com>
2024-04-06 21:01:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function firstChar(text) {
 	text.trim();
-	if(text.length===0 ){
-		return '' ;
+	if(text.length!=0 ){
+		return text[0] ;
 	}
-	return text[0];
+	return '';
 }
 
 // Do not change the code below
